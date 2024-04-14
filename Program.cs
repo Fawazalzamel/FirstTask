b8640f163d7c57cb18362b6e8862a87fb733f966
Fawaz Alzamel <fawazzamel1@gmail.com>
2024-04-14 10:31:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+//Task 1.1 
+
+int thisIsInt = 70;
+double thisIsDouble = 80.8;
+string thisIsString = "Hello";
+bool  thisIsBoolean = false;
+
+Console.WriteLine(thisIsInt);
+Console.WriteLine(thisIsDouble);
+Console.WriteLine(thisIsString);
+Console.WriteLine(thisIsBoolean);
+
+//Task 1.2
+
+int number = 5;
+
+double result = Convert.ToDouble(number);
+Console.WriteLine(result);
+
+
+char grade = 'A';
+Console.WriteLine($"your grade is {grade}");
+
+
+
+
+
+//Task 2.1
+
+var studentName = "Fawaz";
+var GPA = 3.1;
+var favSubject = "Biology";
+
+//Task 2.2
+
+double height = 3.2;
+double width = 5.5;
+
+Console.WriteLine($"The area of the rectangle is { height* width}");
+
+
+var name = "Fawaz";
+ name = "Mubark";
+
+Console.WriteLine(name);
+
+//const String nameOne = "Abdulla";
+//string nameOne = "Ahmed";
+
